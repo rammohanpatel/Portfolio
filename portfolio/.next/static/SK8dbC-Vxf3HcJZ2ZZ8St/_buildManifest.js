a51c6e7f2f0ded82aafb67da85cb597e33583683
Ram Mohan Patel <103207998+rammohanpatel@users.noreply.github.com>
2023-10-28 01:07:57 +0530

    latest

---
+++ portfolio/.next/static/SK8dbC-Vxf3HcJZ2ZZ8St/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/661-4fffa62cc2c8d396.js","static/chunks/pages/index-d39a47bbb928cc6b.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
